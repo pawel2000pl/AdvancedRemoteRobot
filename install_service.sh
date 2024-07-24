fc78c7fadf56a3e5585d408e1648568e17d12ca6
Paweł Bielecki <pawelek24@op.pl>
2024-07-24 08:47:00 +0200

    Added service

--- install_service.sh
+++ install_service.sh
@@ -21,3 +21,26 @@ WantedBy=multi-user.target
 
 systemctl enable init_gpio.service
 systemctl start init_gpio.service
+
+
+
+echo \
+"[Unit]
+Description=Robot network services
+After=network.target
+StartLimitIntervalSec=0
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+User=$USER
+ExecStart=$PWD/start.sh
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/robot-services.service
+
+systemctl enable robot-services.service
+systemctl start robot-services.service
+
