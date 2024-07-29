25a8598c347a4d8ee645e86e8c074ffef72b0a2d
Paweł Bielecki <pawelek24@op.pl>
2024-07-29 14:52:51 +0200

    Added (not finished) audio_service

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+set -e
+
 sudo apt update
 sudo apt full-upgrade -y
-sudo apt install -y curl python3 python3-pip odroid-wiringpi libjpeg8-dev sqlite3
+sudo apt install -y curl cmake python3 python3-pip odroid-wiringpi libjpeg8-dev sqlite3
 
-python3 -m pip install pyserial cherrypy ws4py cython pip-review
-pip-review -a
+python3 -m pip install -U pip
+python3 -m pip install -U setuptools
+python3 -m pip install -U setuptools_scm
+python3 -m pip install pyserial cherrypy ws4py cython numpy
+
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
-
 ./bin/arduino-cli config init
 ./bin/arduino-cli config add board_manager.additional_urls https://files.pololu.com/arduino/package_pololu_index.json
 ./bin/arduino-cli core update-index
--- start.sh
+++ start.sh
@@ -13,6 +13,8 @@ python3 hardware_controller.py &
 HARDWARE_PID=$!
 python3 auth_service.py &
 AUTH_SERVICE_PID=$!
+python3 audio_service.py &
+AUDIO_SERVICE_PID=$!
 
 # Funkcja czyszcząca, która zamyka wszystkie uruchomione skrypty
 cleanup_function() {
@@ -21,10 +23,12 @@ cleanup_function() {
     kill $VIDEO_PID 2>/dev/null
     kill $HARDWARE_PID 2>/dev/null
     kill $AUTH_SERVICE_PID 2>/dev/null
+    kill $AUDIO_SERVICE_PID 2>/dev/null
     wait $STATIC_PID 2>/dev/null
     wait $VIDEO_PID 2>/dev/null
     wait $HARDWARE_PID 2>/dev/null
     wait $AUTH_SERVICE_PID 2>/dev/null
+    wait $AUDIO_SERVICE_PID 2>/dev/null
     echo "Cleanup completed."
 }
 
@@ -35,7 +39,7 @@ trap 'cleanup_function' SIGTERM
 # Funkcja monitorująca działanie skryptów
 monitor_processes() {
     while true; do
-        for PID in $STATIC_PID $VIDEO_PID $HARDWARE_PID $AUTH_SERVICE_PID; do
+        for PID in $STATIC_PID $VIDEO_PID $HARDWARE_PID $AUTH_SERVICE_PID $AUDIO_SERVICE_PID; do
             if ! kill -0 $PID 2>/dev/null; then
                 echo "Process $PID has terminated. Cleaning up remaining processes..."
                 cleanup_function
@@ -54,6 +58,7 @@ wait $STATIC_PID
 wait $VIDEO_PID
 wait $HARDWARE_PID
 wait $AUTH_SERVICE_PID
+wait $AUDIO_SERVICE_PID
 
 # Wywołanie funkcji czyszczącej w przypadku zakończenia wszystkich skryptów
 cleanup_function
